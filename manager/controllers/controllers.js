1e19f5bbc05b7869566e1a1a8c4e45b7b05f0e6e
dudmike <perfect-mira@yandex.ru>
2016-04-18 14:03:07 +0200

    added service for customers

--- manager/controllers/controllers.js
+++ manager/controllers/controllers.js
@@ -1,7 +1,5 @@
-app.controller('CustomersController', function ($scope, customersService) {
+manager.controller('CustomersController', function ($scope, customersService) {
 
-    //I like to have an init() for controllers that need to perform some initialization. Keeps things in
-    //one place...not required though especially in the simple example below
     init();
 
     function init() {
