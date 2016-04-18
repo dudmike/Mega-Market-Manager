ac2ed601b53a4bf7953d3a6167b58e0a97cde3d5
dudmike <perfect-mira@yandex.ru>
2016-04-18 17:20:47 +0200

    added controllers(OrdersController,OrderChildController)

--- manager/controllers/controllers.js
+++ manager/controllers/controllers.js
@@ -20,3 +20,41 @@ manager.controller('CustomersController', function ($scope, customersService) {
         customersService.deleteCustomer(id);
     };
 });
+
+manager.controller('OrdersController', function ($scope, customersService) {
+    $scope.customers = [];
+
+    init();
+
+    function init() {
+        $scope.customers = customersService.getCustomers();
+    }
+});
+
+manager.controller('OrderChildController', function ($scope) {
+    $scope.orderby = 'product';
+    $scope.reverse = false;
+    $scope.ordersTotal = 0;
+
+    init();
+
+    function init() {
+        if ($scope.customer && $scope.customer.orders) {
+            var total = 0;
+            for (var i = 0; i < $scope.customer.orders.length; i++) {
+                var order = $scope.customer.orders[i];
+                total += order.orderTotal;
+            }
+            $scope.ordersTotal = total;
+        }
+    }
+
+    $scope.setOrder = function (orderby) {
+        if (orderby === $scope.orderby)
+        {
+            $scope.reverse = !$scope.reverse;
+        }
+        $scope.orderby = orderby;
+    };
+
+});
