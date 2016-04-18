ade77536e8d4bf602f92ed2cc80c76d9644af292
dudmike <perfect-mira@yandex.ru>
2016-04-18 13:53:38 +0200

    renamed controller.js into controllers.js

---
+++ manager/controllers/controllers.js
@@ -0,0 +1,24 @@
+app.controller('CustomersController', function ($scope, customersService) {
+
+    //I like to have an init() for controllers that need to perform some initialization. Keeps things in
+    //one place...not required though especially in the simple example below
+    init();
+
+    function init() {
+        $scope.customers = customersService.getCustomers();
+    }
+
+    $scope.insertCustomer = function () {
+        var firstName = $scope.newCustomer.firstName;
+        var lastName = $scope.newCustomer.lastName;
+        var city = $scope.newCustomer.city;
+        customersService.insertCustomer(firstName, lastName, city);
+        $scope.newCustomer.firstName = '';
+        $scope.newCustomer.lastName = '';
+        $scope.newCustomer.city = '';
+    };
+
+    $scope.deleteCustomer = function (id) {
+        customersService.deleteCustomer(id);
+    };
+});
